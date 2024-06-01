afc9fa432a16232902c4c1a521d3c9a60a20696e
Fabian Cid <fsk16@hotmail.cl>
2024-06-01 03:32:59 -0400

    (feat) implementacion swagger

--- src/main/java/com/ejercicio/usuarios/UsuariosApplication.java
+++ src/main/java/com/ejercicio/usuarios/UsuariosApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
 @SpringBootApplication
 public class UsuariosApplication {
 
@@ -15,6 +18,12 @@ public class UsuariosApplication {
 	@Bean
 	public ModelMapper modelMapper() {
 		return new ModelMapper();
+	}
+
+	@Bean
+	public OpenAPI customOpenAPI() {
+		return new OpenAPI().info(new Info().title("API Creacion de Usuarios")
+				.description("API resolucion de Ejercicio Creacion De Usuarios").version("0.0.1"));
 	}
 
 }
